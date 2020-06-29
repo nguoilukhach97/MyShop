e8d8f3276704e7d9afe3d1ddbd3cb47fee0274dc
hungth230297@gmail.com <hungth230297@gmail.com>
2020-06-29 15:27:07 +0700

    create contructure

---
+++ MyShop.Utilities/Exceptions/ShopException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyShop.Utilities.Exceptions
+{
+    public class ShopException : Exception
+    {
+        public ShopException()
+        {
+
+        }
+        public ShopException(string message):base(message)
+        {
+
+        }
+        public ShopException(string message, Exception exception) : base(message,exception)
+        {
+
+        }
+    }
+}
